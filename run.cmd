627317ce30749b172c1cc1b7981a9fc303456549
Pavel <5209166+pas2k@users.noreply.github.com>
2026-06-09 16:39:53 +0200

    fix: proper env for the launcher

--- run.cmd
+++ run.cmd
@@ -2,5 +2,6 @@
 
 if not defined PYTHON (set PYTHON="%LocalAppData%\Microsoft\WindowsApps\py.exe")
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
+if not defined VIRTUAL_ENV (set "VIRTUAL_ENV=%~dp0venv")
 
 %PYTHON% -m gui.backend
